36b6fc991ae9ba591b1594fc6112486e0a48dfcf
Abir Chouabi <hachaniabir@yahoo.fr>
2022-06-22 21:30:55 +0200

    etapes 1,2,3 et 4

---
+++ front/scripts/index.js
@@ -0,0 +1,43 @@
+
+/******************* Afficher les produits sur la page d'accueil*******************/
+items()
+async function items(){
+    const products = await getproducts()
+    console.table(products)
+    for(let i = 0; i< products.length; i++){
+        const product=products[i];
+        displayproduct(product)
+    }
+}
+
+/***Fonction pour récuperer les différents produits***/
+
+function getproducts() {
+    
+    return fetch("http://localhost:3000/api/products")
+    
+    .then(function(res){
+        return res.json();
+    })
+    .then(function(products) {
+        return products
+    })
+    .catch(function(error) {
+        alert(error)
+    })
+}
+
+/***Fonction pour afficher un produit***/
+
+function displayproduct(product){
+    document.getElementById("items").innerHTML +=`
+    
+          <a href="./product.html?id=${product._id}">
+            <article>
+              <img src="${product.imageUrl}" alt="${product.altTxt}">
+              <h3 class="productName">${product.name}</h3>
+              <p class="productDescription">${product.description}</p>
+            </article>
+          </a>
+        `
+}
